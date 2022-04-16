5794c8cb107b5bfddb4142f7d13d3c1909aad63b
Taras Shkodenko <taras.shkodenko@blackbit.de>
2022-04-17 01:11:54 +0300

    Improved Google reCaptcha v.2 spam check in contact.php

--- contact.php
+++ contact.php
@@ -8,13 +8,38 @@
 
 $mailRes = false;
 if (!empty($_POST)) {
-    $msg = date('r') . ' $_POST data: ' . var_export($_POST, 1) . PHP_EOL . ' $_SERVER: ' . var_export($_SERVER, 1) . PHP_EOL;
-    $mailRes = mail('podlom@gmail.com', $_SERVER['HTTP_HOST'] . ' contact form', $msg);
-	$logFile = dirname(__FILE__) . '/log/contact.txt';
-    if (file_exists($logFile) && is_writeable($logFile)) {
-        error_log($msg, 3, $logFile);
-    } else {
-        error_log($msg);
+    if (isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])) {
+        // @see: https://developers.google.com/recaptcha/docs/verify
+        $postdata = http_build_query(
+            [
+                'secret' => '6LeqNXwfAAAAAPaxwaPYT98VhbQr_1oEstNblqi_',
+                'response' => $_POST['g-recaptcha-response'],
+                // 'remoteip' => 'Optional. The user's IP address.',
+            ]
+        );
+        $opts = [
+            'http' =>
+                [
+                    'method' => 'POST',
+                    'header' => 'Content-type: application/x-www-form-urlencoded',
+                    'content' => $postdata,
+                ],
+        ];
+        $context = stream_context_create($opts);
+        $result = file_get_contents('https://www.google.com/recaptcha/api/siteverify', false, $context);
+        //
+        if ($result) {
+            $msg = date('r') . ' $_POST data: ' . var_export($_POST, 1) . PHP_EOL . ' $_SERVER: ' . var_export($_SERVER, 1) . PHP_EOL;
+            $mailRes = mail('podlom@gmail.com', $_SERVER['HTTP_HOST'] . ' contact form', $msg);
+            $logFile = dirname(__FILE__) . '/log/contact.txt';
+            if (file_exists($logFile) && is_writeable($logFile)) {
+                error_log($msg, 3, $logFile);
+            } else {
+                error_log($msg);
+            }
+        } else {
+            // TODO: SPAM
+        }
     }
 }
 header("Content-type: application/json");
