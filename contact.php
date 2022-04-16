6a55e88253a92ae4aaf1c4e68f91ae37dd8c3a78
Taras Shkodenko <taras.shkodenko@blackbit.de>
2022-04-17 00:41:06 +0300

    Improved contact.php

--- contact.php
+++ contact.php
@@ -8,11 +8,13 @@
 
 $mailRes = false;
 if (!empty($_POST)) {
-    $msg = date('r') . ' $_POST data: ' . var_export($_POST, 1) . PHP_EOL . '$_SERVER: ' . var_export($_SERVER, 1) . PHP_EOL;
+    $msg = date('r') . ' $_POST data: ' . var_export($_POST, 1) . PHP_EOL . ' $_SERVER: ' . var_export($_SERVER, 1) . PHP_EOL;
     $mailRes = mail('podlom@gmail.com', $_SERVER['HTTP_HOST'] . ' contact form', $msg);
 	$logFile = dirname(__FILE__) . '/log/contact.txt';
     if (file_exists($logFile) && is_writeable($logFile)) {
         error_log($msg, 3, $logFile);
+    } else {
+        error_log($msg);
     }
 }
 header("Content-type: application/json");
